1881ad03e5b0ca2380d3be384777a7efd72f0ce1
boo-0x <boo_0x@tutanota.com>
2023-01-04 13:20:46 +0100

    Contract query resolver

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,7 +1,30 @@
+import "reflect-metadata";
+import { Field, ObjectType, Query, Resolver } from 'type-graphql'
+import type { EntityManager } from 'typeorm'
 import { VerificationRequestResolver } from "./verificationRequest";
 import { VerifiedContractResolver } from "./verifiedContract";
+import { ContractResolver } from "./contract";
+
+@ObjectType()
+export class Ping {
+  @Field(() => String, { nullable: false })
+  message!: string
+
+  constructor(message: string) { this.message = message }
+}
+
+@Resolver()
+export class PingResolver {
+  constructor(private tx: () => Promise<EntityManager>) {}
+
+  @Query(() => Ping)
+  async ping(): Promise<Ping> {
+    return new Ping(`Custom API extension works!`)
+  }
+}
 
 export {
   VerificationRequestResolver,
-  VerifiedContractResolver
+  VerifiedContractResolver,
+  ContractResolver
 }
